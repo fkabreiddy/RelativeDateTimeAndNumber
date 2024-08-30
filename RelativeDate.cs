d501193c2a6d0dce63430d108c99f8f3fe2b176c
fkabreiddy <breiddysubzero@gmai..com>
2024-08-30 18:29:52 -0400

    first commit

---
+++ RelativeDate.cs
@@ -0,0 +1,51 @@
+﻿namespace RelativeDateAndNumbers;
+
+public static class RelativeDate
+{
+    public static string AsRelative(this DateTime dateTime, string language = "us", bool shortForm = true)
+    {
+        var timeSpan = DateTime.Now - dateTime;
+
+        if (language.ToLower() == "es")
+        {
+
+            if (timeSpan.TotalSeconds < 60)
+                return $"{(int)timeSpan.TotalSeconds} {(shortForm ? "s" : " segundos")}"; // Segundos
+
+            if (timeSpan.TotalMinutes < 60)
+                return $"{(int)timeSpan.TotalMinutes} {(shortForm ? "m" : " minutos")}"; // Minutos
+
+            if (timeSpan.TotalHours < 24)
+                return $"{(int)timeSpan.TotalHours} {(shortForm ? "h" : " horas")}"; // Horas
+
+            if (timeSpan.TotalDays < 30)
+                return $"{(int)timeSpan.TotalDays} {(shortForm ? "d" : " días")}"; // Días
+
+            if (timeSpan.TotalDays < 365)
+                return $"{(int)(timeSpan.TotalDays / 30)} {(shortForm ? "M" : " Meses")}"; // Meses
+
+            return $"{(int)(timeSpan.TotalDays / 365)} {(shortForm ? "Y" : " Años")}"; // Años
+        }
+        
+
+        if (timeSpan.TotalSeconds < 60)
+            return $"{(int)timeSpan.TotalSeconds} {(shortForm ? "s" : " seconds")}"; // Segundos
+
+        if (timeSpan.TotalMinutes < 60)
+            return $"{(int)timeSpan.TotalMinutes} {(shortForm ? "m" : " minutes")}"; // Minutos
+
+        if (timeSpan.TotalHours < 24)
+            return $"{(int)timeSpan.TotalHours} {(shortForm ? "h" : " hours")}"; // Horas
+
+        if (timeSpan.TotalDays < 30)
+            return $"{(int)timeSpan.TotalDays} {(shortForm ? "d" : " days")}"; // Días
+
+        if (timeSpan.TotalDays < 365)
+            return $"{(int)(timeSpan.TotalDays / 30)} {(shortForm ? "M" : " Months")}"; // Meses
+
+        return $"{(int)(timeSpan.TotalDays / 365)} {(shortForm ? "Y" : " Years")}"; // Años
+    }
+
+
+   
+}
---
+++ RelativeNumber.cs
@@ -0,0 +1,29 @@
+﻿namespace RelativeDateAndNumbers;
+
+public static class RelativeNumber
+{
+    public static string AsRelative(this int num, string language = "us", bool shortForm = true)
+    {
+        
+        if (shortForm)
+        {
+            if (num >= 1_000_000_000)
+                return $"{num / 1_000_000_000} {(language.ToLower() == "es" ? "B" : "B")}";
+            if (num >= 1_000_000)
+                return $"{num / 1_000_000} {(language.ToLower() == "es" ? "M" : "M")}";
+            if (num >= 1_000)
+                return $"{num / 1_000} {(language.ToLower() == "es" ? "k" : "k")}";
+        }
+        else
+        {
+            if (num >= 1_000_000_000)
+                return $"{num / 1_000_000_000} {(language.ToLower() == "es" ? " billones" : " billion")}";
+            if (num >= 1_000_000)
+                return $"{num / 1_000_000} {(language.ToLower() == "es" ? " millones" : " million")}";
+            if (num >= 1_000)
+                return $"{num / 1_000} {(language.ToLower() == "es" ? " mil" : " thousand")}";
+        }
+        return num.ToString();
+    }
+
+}
